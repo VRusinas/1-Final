c30988bdbc664c4ccb9bf107599bb6f9f16cdef6
VRusinas <vilius1206@gmail.com>
2021-09-16 14:36:03 +0300

    swiper install

--- script.js
+++ script.js
@@ -24,3 +24,25 @@ for (var i = 0; i < topLinks.length; i++) {
       this.classList.add("active");
     }
 }
+
+
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+});
