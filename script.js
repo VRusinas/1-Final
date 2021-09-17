b0e9c04652978def21f53b0584615ccc604d16b7
VRusinas <vilius1206@gmail.com>
2021-09-17 20:30:19 +0300

    Scroll animation

--- script.js
+++ script.js
@@ -61,3 +61,18 @@ const swiper = new Swiper('.swiper', {
     }
   }
 });
+
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+  $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
