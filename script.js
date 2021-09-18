71e82814f42c8507b345aba9cdeb44e9c601ca90
VRusinas <vilius1206@gmail.com>
2021-09-18 20:52:20 +0300

    hover-image-text-fix

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ $(document).ready(function(){
   }
 }
 
-function openCity(evt, cityName) {
+function openCity(evt, choice) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
@@ -138,7 +138,7 @@ function openCity(evt, cityName) {
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active2", "");
   }
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(choice).style.display = "block";
   evt.currentTarget.className += " active2";
 
 }
