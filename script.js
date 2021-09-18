0c5a67467d8ad3bfeb3b53fb95e72eccce3ff9ba
VRusinas <vilius1206@gmail.com>
2021-09-18 13:48:56 +0300

    Tab development

--- script.js
+++ script.js
@@ -126,3 +126,18 @@ $(document).ready(function(){
     alert("Forma sukurta.")
   }
 }
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active2", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active2";
+}
+document.getElementById("defaultOpen").click();
